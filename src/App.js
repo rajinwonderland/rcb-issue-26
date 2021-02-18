1b117db22800aef8c9464ffadb596e71bff880eb
Raj K Singh <raj@novvuminc.com>
2021-02-17 17:23:56 -0800

    Added block with static text Checking if cra build styles affect static text props set as well

--- src/App.js
+++ src/App.js
@@ -37,6 +37,32 @@ export default function App() {
         language="javascript"
         codeBlock
       />
+      <small>with dynamic text</small>
+      <br />
+      <br />
+      <CopyBlock
+        showLineNumbers={false}
+        theme={toggleCodeTheme}
+        text={`function recursive(arrLength) {
+        var t0 = performance.now();
+        var firstNum = arr[arr.length - 2];
+        var secondNum = arr[arr.length - 1];
+        var nNum = firstNum + secondNum;
+        arr.push(nNum);
+        firstNum = secondNum;
+        secondNum = nNum;
+      
+        if (arr.length < arrLength) {
+          recursive(arrLength);
+        }
+      
+        var t1 = performance.now();
+        return (t1 - t0) * 1000;
+      }`}
+        language="javascript"
+        codeBlock
+      />
+      <small>with static text</small>
       <br />
       <h2>Iterative Example</h2>
       <CopyBlock
